a29d72f0d311b6aa1e4efcc1416cdd089abf890b
redpois0n <red@cmail.nu>
2015-05-22 10:45:58 +0200

    First test

--- src/icolib/Ico.java
+++ src/icolib/Ico.java
@@ -1,11 +1,19 @@
 package icolib;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
 
 public class Ico {
+	
+	private List<BufferedImage> images = new ArrayList<BufferedImage>();
 	
 	public Ico(File file) throws Exception {
 		this(new FileInputStream(file));
@@ -21,11 +29,15 @@ public class Ico {
 		assert dis.readShort() == 0; // always 0
 		assert dis.readShort() == 1; // .ico
 		
-		short images = dis.readShort();
+		short totalImages = dis.readShort();
 		
-		for (short i = 0; i < images; i++) {
+		int pos = 6;
+		
+		for (short i = 0; i < totalImages; i++) {
 			int width = dis.readByte();
 			int height = dis.readByte();
+			
+			pos += 2;
 			
 			if (width == 0) {
 				width = 256;
@@ -42,13 +54,24 @@ public class Ico {
 			short colorpanes = dis.readShort();
 			short bitsperpixel = dis.readShort();
 			
+			pos += 6;
+			
 			int length = dis.readInt();
 			int offset = dis.readInt();
+			
+			pos += 8;
+			
 			dis.reset();
 			dis.skip(offset);
 			
 			byte[] image = new byte[length];
 			dis.readFully(image);
+			
+			ByteArrayInputStream bais = new ByteArrayInputStream(image);
+			images.add(ImageIO.read(bais));
+			
+			dis.reset();
+			dis.skip(pos);
 		}
 	}
 
