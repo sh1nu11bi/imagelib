8774118d33a86e731cb7fd94dcbcb2e04da0ab21
redpois0n <red@cmail.nu>
2015-05-22 10:30:28 +0200

    constructors, begin parsing

--- src/icolib/Ico.java
+++ src/icolib/Ico.java
@@ -1,9 +1,29 @@
 package icolib;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 public class Ico {
 	
-	public Ico() {
+	public Ico(File file) throws Exception {
+		this(new FileInputStream(file));
+	}
+	
+	public Ico(InputStream in) throws Exception {
+		this(new DataInputStream(in));
+	}
+	
+	public Ico(DataInputStream dis) throws Exception {
+		assert dis.readShort() == 0; // always 0
+		assert dis.readShort() == 1; // .ico
 		
+		short images = dis.readShort();
+		
+		for (short i = 0; i < images; i++) {
+			
+		}
 	}
 
 }
