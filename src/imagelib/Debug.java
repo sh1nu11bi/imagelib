c473da7fe0fb85b91e708e285b43cda4aaa20709
redpois0n <red@cmail.nu>
2015-05-22 13:08:41 +0200

    Encodes and decodes in Debug class

--- src/imagelib/Debug.java
+++ src/imagelib/Debug.java
@@ -1,13 +1,18 @@
 package imagelib;
 
 import imagelib.ico.IcoDecoder;
+import imagelib.ico.IcoEncoder;
 
 import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.imageio.ImageIO;
 
 public class Debug {
 	
 	public static void main(String[] args) {
 		try {
+			IcoEncoder.encode(ImageIO.read(new File("test.png")), new FileOutputStream("testout.ico"));
 			IcoDecoder.decode(new File("test.ico"));
 		} catch (Exception e) {
 			e.printStackTrace();
